a800014d4d1b687a3df9bfe77e240908ad360987
Ubuntu <user@ubuntu.com>
2022-10-22 09:06:59 -0700

    tree view

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Page } from "@geist-ui/react";
+import { Trace } from "./Trace";
+
+const App = (props) => {
+  return (
+    <Page>
+      <Page.Header>
+        <h2>Header</h2>
+      </Page.Header>
+      <Page.Content>
+        <h2>Hello, Everyone.</h2>
+        <Trace />
+      </Page.Content>
+      <Page.Footer>
+        <h2>Footer</h2>
+      </Page.Footer>
+    </Page>
+  );
+};
+export default App;
