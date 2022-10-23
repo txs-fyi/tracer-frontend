4857ea5e064f5eea5d9aaeafb1b7ddb44d08495b
libevm <libevm32@gmail.com>
2022-10-23 15:01:27 -0700

    ok

--- src/pages/TracePage.js
+++ src/pages/TracePage.js
@@ -17,6 +17,11 @@ function getUnknownFunctionSignatures(decoder, stackTrace) {
   const { input, calls } = stackTrace;
   const innerCalls = calls || [];
 
+  // not a call, but probably a LOG, SSTORE opcode
+  if (input === undefined) {
+    return [];
+  }
+
   try {
     // Decoder successfully decoded the data,
     // its a known function signature
@@ -46,6 +51,11 @@ function getUnkownAddresses(knownAddresses, stackTrace) {
   const { from, to, calls } = stackTrace;
 
   let unknowns = [];
+
+  // not a call, but probably a LOG, SSTORE opcode
+  if (from === undefined || to === undefined) {
+    return [];
+  }
 
   // If not found
   if (!knownAddresses[from.toLowerCase()]) {
@@ -90,11 +100,42 @@ function TraceViewer(stackTrace) {
 
   const prettyGas = parseInt(gas, 16);
 
+  /*
+    #BB86FC - CALL
+    #3700B3 - DELEGATECALL
+    #03DAC6 - STATICALL
+    #018786 - CREATE
+    #CF6679 - REVERT
+  */
+
+  // White by default
+  let colorType = "#FFF";
+  if (type === "CALL") {
+    colorType = "#BB86FC";
+  } else if (type === "DELEGATECALL") {
+    colorType = "#3700B3";
+  } else if (type === "STATICCALL") {
+    colorType = "#03DAC6";
+  } else if (type === "CREATE" || type === "CREATE2") {
+    colorType = "#018786";
+  }
+
+  if (type === "SSTORE" || type === "SLOAD" || type.startsWith("LOG")) {
+    return (
+      <li key={`${JSON.stringify(stackTrace)}`}>
+        <span style={{ color: colorType }}>[{type}]</span>
+        {prettyValueStr}
+        {prettyAddress || to}::{prettyInput || input}
+      </li>
+    );
+  }
+
   return (
-    <li key={`${isNaN(prettyGas) ? "0" : prettyGas.toString()}`}>
+    <li key={`${type}-${isNaN(prettyGas) ? "0" : prettyGas.toString()}`}>
       <details open>
         <summary>
-          [{type}]{prettyValueStr}
+          <span style={{ color: colorType }}>[{type}]</span>
+          {prettyValueStr}
           {prettyAddress || to}::{prettyInput || input}
         </summary>
         <ul>
@@ -107,16 +148,13 @@ function TraceViewer(stackTrace) {
           {
             // Sending ETH doesn't return value it seems
             output === undefined && error === undefined && (
-              <li key={`return-${parseInt(gas, 16).toString()}`}>
+              <li key={`return-${type}-${parseInt(gas, 16).toString()}`}>
                 return [0x]
               </li>
             )
           }
           {error !== undefined && (
-            <li
-              className="error-li"
-              key={`revert-${parseInt(gas, 16).toString()}`}
-            >
+            <li key={`revert-${type}-${parseInt(gas, 16).toString()}`}>
               reverted [{error}]
             </li>
           )}
@@ -127,7 +165,7 @@ function TraceViewer(stackTrace) {
 }
 
 // Make the contract traces readable
-function formatTraceTree(decoder, knownContractAddresses, stackTrace) {
+function formatExecutionTrace(decoder, knownContractAddresses, stackTrace) {
   let prettyInput = null;
   let prettyAddress = null;
   let prettyOutput = null;
@@ -181,7 +219,7 @@ function formatTraceTree(decoder, knownContractAddresses, stackTrace) {
     prettyInput,
     prettyOutput,
     calls: (stackTrace.calls || []).map((x) =>
-      formatTraceTree(decoder, knownContractAddresses, x)
+      formatExecutionTrace(decoder, knownContractAddresses, x)
     ),
   };
 }
@@ -199,12 +237,12 @@ export const TracePage = () => {
     []
   );
   const [invalidTxHash, setInvalidTxHash] = useState(false);
-  const [callTrace, setCallTrace] = useState(null);
+  const [executionTrace, setExecutionTrace] = useState(null);
   const [hasUpdatedLocalStorage, setHasUpdatedLocalStorage] = useState(false);
 
   const getTraces = useCallback(async () => {
     const traceData = await fetch(
-      `http://localhost:3001/eth/${txhash}?callTrace=true`,
+      `http://localhost:3001/eth/${txhash}?executionTrace=true`,
       {
         method: "GET",
       }
@@ -212,134 +250,135 @@ export const TracePage = () => {
       .then((x) => x.json())
       .catch(() => null);
 
-    if (traceData === null || traceData.callTrace === undefined) {
+    if (traceData === null || traceData.executionTrace === undefined) {
       setInvalidTxHash(true);
       return;
     }
 
-    setCallTrace(traceData.callTrace);
+    setExecutionTrace(traceData.executionTrace);
   }, [txhash]);
 
-  const getAddressAndSignaturesAndUpdateCallTrace = useCallback(async () => {
-    // Debounce
-    if (hasUpdatedLocalStorage) return;
-    setHasUpdatedLocalStorage(true);
+  const getAddressAndSignaturesAndUpdateExecutionTrace =
+    useCallback(async () => {
+      // Debounce
+      if (hasUpdatedLocalStorage) return;
+      setHasUpdatedLocalStorage(true);
 
-    // Get function signatures
-    const unknown4s = getUniqueUnkownFunctionSignatures(
-      ifaceDecoder,
-      callTrace
-    );
-    const hexSigsRes = await Promise.all(
-      unknown4s.map((x) =>
-        fetch(
-          `https://www.4byte.directory/api/v1/signatures/?format=json&hex_signature=${x}`,
-          {
-            method: "GET",
-          }
-        )
-          .then((x) => x.json())
-          .catch(() => null)
-      )
-    );
-    const hexSigsResFlattened = hexSigsRes
-      .filter((x) => x !== null)
-      .map((x) => x.results)
-      .flat();
-    const newSignatures = hexSigsResFlattened.map(
-      (x) => "function " + x.text_signature
-    );
-
-    // Save to localStorage
-    const newKnownSignatures = [...knownSignatures, ...newSignatures];
-
-    // Set a new decoder
-    const newIFaceDecoder = new ethers.utils.Interface([
-      ...ifaceDecoder.format(),
-      ...newKnownSignatures,
-    ]);
-
-    // Attempt to get contract address name, rate limited to 5 every 1 second
-    const unknownAddresses = getUniqueUnknownAddresses(
-      knownContractAddresses,
-      callTrace
-    );
-    let addressesSourceCode = [];
-    for (let i = 0; i < unknownAddresses.length; i += 5) {
-      const curSourceCodes = await Promise.all(
-        unknownAddresses.slice(i, i + 5).map((x) =>
+      // Get function signatures
+      const unknown4s = getUniqueUnkownFunctionSignatures(
+        ifaceDecoder,
+        executionTrace
+      );
+      const hexSigsRes = await Promise.all(
+        unknown4s.map((x) =>
           fetch(
-            `https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${x}&apikey=CAXVKF8PKZ3GV79PYYNPTUQ2J1TVKRPP51`,
+            `https://www.4byte.directory/api/v1/signatures/?format=json&hex_signature=${x}`,
             {
               method: "GET",
             }
           )
             .then((x) => x.json())
-            .catch(() => {
-              return { error: true };
-            })
+            .catch(() => null)
         )
       );
-      addressesSourceCode = [...addressesSourceCode, ...curSourceCodes];
-
-      // Rate limit of 5 req per second
-      if (unknownAddresses.length >= 5) {
-        await sleep(1010);
-      }
-
-      // Exrtract out source code name (very hacky yes)
-      const addressesNames = addressesSourceCode.map((x) => {
-        let name = null;
-        try {
-          name = x.result[0]["ContractName"];
-        } catch (e) {}
-        if (name === "") {
-          name = null;
-        }
-        return name;
-      });
-
-      // New key value
-      const addressesWithNames = unknownAddresses
-        .map((_, idx) => {
-          return [unknownAddresses[idx].toLowerCase(), addressesNames[idx]];
-        })
-        .reduce((acc, x) => {
-          if (x[1] === null) return acc;
-          return {
-            ...acc,
-            [x[0]]: x[1],
-          };
-        }, {});
-
-      // Save new Addresses
-      const newKnownContractAddresses = {
-        ...knownContractAddresses,
-        ...addressesWithNames,
-      };
-
-      // Re-format trace data with new data
-      const newStackTrace = formatTraceTree(
-        newIFaceDecoder,
-        newKnownContractAddresses,
-        callTrace
+      const hexSigsResFlattened = hexSigsRes
+        .filter((x) => x !== null)
+        .map((x) => x.results)
+        .flat();
+      const newSignatures = hexSigsResFlattened.map(
+        (x) => "function " + x.text_signature
       );
 
-      // Finally set all the call data after we've parsed all the
-      setCallTrace(newStackTrace);
-      setIFaceDecoder(newIFaceDecoder);
-      setKnownContractAddresses(newKnownContractAddresses);
-      setKnownSignatures(newKnownSignatures);
-    }
-  }, [
-    hasUpdatedLocalStorage,
-    setKnownContractAddresses,
-    setKnownSignatures,
-    ifaceDecoder,
-    knownContractAddresses,
-    knownSignatures,
-    callTrace,
-  ]);
+      // Save to localStorage
+      const newKnownSignatures = [...knownSignatures, ...newSignatures];
+
+      // Set a new decoder
+      const newIFaceDecoder = new ethers.utils.Interface([
+        ...ifaceDecoder.format(),
+        ...newKnownSignatures,
+      ]);
+
+      // Attempt to get contract address name, rate limited to 5 every 1 second
+      const unknownAddresses = getUniqueUnknownAddresses(
+        knownContractAddresses,
+        executionTrace
+      );
+      let addressesSourceCode = [];
+      for (let i = 0; i < unknownAddresses.length; i += 5) {
+        const curSourceCodes = await Promise.all(
+          unknownAddresses.slice(i, i + 5).map((x) =>
+            fetch(
+              `https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${x}&apikey=CAXVKF8PKZ3GV79PYYNPTUQ2J1TVKRPP51`,
+              {
+                method: "GET",
+              }
+            )
+              .then((x) => x.json())
+              .catch(() => {
+                return { error: true };
+              })
+          )
+        );
+        addressesSourceCode = [...addressesSourceCode, ...curSourceCodes];
+
+        // Rate limit of 5 req per second
+        if (unknownAddresses.length >= 5) {
+          await sleep(1010);
+        }
+
+        // Exrtract out source code name (very hacky yes)
+        const addressesNames = addressesSourceCode.map((x) => {
+          let name = null;
+          try {
+            name = x.result[0]["ContractName"];
+          } catch (e) {}
+          if (name === "") {
+            name = null;
+          }
+          return name;
+        });
+
+        // New key value
+        const addressesWithNames = unknownAddresses
+          .map((_, idx) => {
+            return [unknownAddresses[idx].toLowerCase(), addressesNames[idx]];
+          })
+          .reduce((acc, x) => {
+            if (x[1] === null) return acc;
+            return {
+              ...acc,
+              [x[0]]: x[1],
+            };
+          }, {});
+
+        // Save new Addresses
+        const newKnownContractAddresses = {
+          ...knownContractAddresses,
+          ...addressesWithNames,
+        };
+
+        // Re-format trace data with new data
+        const newStackTrace = formatExecutionTrace(
+          newIFaceDecoder,
+          newKnownContractAddresses,
+          executionTrace
+        );
+
+        // Finally set all the call data after we've parsed all the
+        setExecutionTrace(newStackTrace);
+        setIFaceDecoder(newIFaceDecoder);
+        setKnownContractAddresses(newKnownContractAddresses);
+        setKnownSignatures(newKnownSignatures);
+      }
+    }, [
+      hasUpdatedLocalStorage,
+      setKnownContractAddresses,
+      setKnownSignatures,
+      ifaceDecoder,
+      knownContractAddresses,
+      knownSignatures,
+      executionTrace,
+    ]);
 
   useEffect(() => {
     // Initialize decoder
@@ -353,37 +392,40 @@ export const TracePage = () => {
     }
 
     // Get traces
-    if (!invalidTxHash && callTrace === null) {
+    if (!invalidTxHash && executionTrace === null) {
       getTraces();
     }
-    if (!invalidTxHash && callTrace) {
+    if (!invalidTxHash && executionTrace) {
     }
-  }, [getTraces, ifaceDecoder, knownSignatures, invalidTxHash, callTrace]);
+  }, [getTraces, ifaceDecoder, knownSignatures, invalidTxHash, executionTrace]);
 
   useEffect(() => {
     if (invalidTxHash) return;
-    if (callTrace === null) return;
+    if (executionTrace === null) return;
 
     // Get 4byte signatures
     if (!hasUpdatedLocalStorage) {
-      getAddressAndSignaturesAndUpdateCallTrace();
+      getAddressAndSignaturesAndUpdateExecutionTrace();
     }
   }, [
-    getAddressAndSignaturesAndUpdateCallTrace,
+    getAddressAndSignaturesAndUpdateExecutionTrace,
     hasUpdatedLocalStorage,
     invalidTxHash,
-    callTrace,
+    executionTrace,
   ]);
 
   return (
     <GenericPage>
-      {callTrace === null && invalidTxHash && <Text h2>Invalid txhash</Text>}
-      {callTrace !== null && (
+      {executionTrace === null && invalidTxHash && (
+        <Text h2>Invalid txhash</Text>
+      )}
+      <Text h4>Execution Trace</Text>
+      {executionTrace !== null && (
         <ul className="tree">
           <li key="root">
             <details open>
-              <summary>[Sender]{callTrace.from}</summary>
-              <ul>{TraceViewer(callTrace)}</ul>
+              <summary>[Sender]{executionTrace.from}</summary>
+              <ul>{TraceViewer(executionTrace)}</ul>
             </details>
           </li>
         </ul>
